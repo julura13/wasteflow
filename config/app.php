4599f12a82c1c5876e1e37ab663da306e6e62175
julura13 <juan@julura.co.za>
2026-08-18 20:16:26 +0200

    Add release note for Ctrl+P filename improvement
    Bumps app version to 1.10.11 to match.

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'version' => env('APP_VERSION', '1.10.10'),
+    'version' => env('APP_VERSION', '1.10.11'),
 
     /*
     |--------------------------------------------------------------------------
